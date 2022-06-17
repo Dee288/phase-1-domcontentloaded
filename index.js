ef73508054fa05e34af7abfb9ac2cc98794d58df
dee288 <dianamajala10@gmail.com>
2022-06-17 06:10:57 +0300

    commit tree 1333381e1fc6fe357189223ef99f305d70608db9

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+  textUpdate()
+} );
+
+function textUpdate(){
+const texr= document.getElementById( "text" )
+.innerHTML = "This is really cool!";}
